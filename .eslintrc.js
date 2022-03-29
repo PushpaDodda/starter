29309afe62724f55b7b23b55164710f45713f59c
PUSHPA <pdodda-zem@zushealth.com>
2022-03-29 13:30:13 +0530

    add test coverage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:testing-library/react"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": ['./tsconfig.json'], // Specify it only for TypeScript files
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:testing-library/react",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"], // Specify it only for TypeScript files
+    ecmaFeatures: {
+      jsx: true,
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "testing-library"
-    ],
-    "rules": {
-        "react/prop-types" : "off",
-        "react/react-in-jsx-scope" : "off",
-        "@typescript-eslint/no-floating-promises":"off"
-    }
-}
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "testing-library", "jest"],
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+  },
+};
